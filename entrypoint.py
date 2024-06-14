36726a45c4eddfe99eaf43ffe7b39c726d50b9f6
pm8k <matthew.russell@getcatalyst.io>
2024-06-14 11:03:36 -0500

    switching to dandir

--- entrypoint.py
+++ entrypoint.py
@@ -2,5 +2,5 @@ from qtr_pairing_process.ui_manager import UiManager
 from qtr_pairing_process.constants import DEFAULT_COLOR_MAP, MATT_DIR, DAN_DIR
 
 if __name__ == '__main__':
-    ui_manager = UiManager(color_map=DEFAULT_COLOR_MAP, directory=MATT_DIR)
+    ui_manager = UiManager(color_map=DEFAULT_COLOR_MAP, directory=DAN_DIR)
     ui_manager.create_ui()
