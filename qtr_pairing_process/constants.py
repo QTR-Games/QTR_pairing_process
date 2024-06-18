c94dadf2ce3855a0f2c6864e2fe95d6e1ba296ed
quotemyname <dan.raven@thomsonreuters.com>
2024-06-18 14:31:16 -0400

    Adding Constants
    Adding constants to support adding scenario grid.

--- qtr_pairing_process/constants.py
+++ qtr_pairing_process/constants.py
@@ -7,6 +7,24 @@ DEFAULT_COLOR_MAP = {
     '5': 'deepskyblue'
 }
 
-MATT_DIR = '.'
-DAN_DIR = os.getcwd()
+# MATT_DIR = '.'
 # DAN_DIR = "C:/Users/Daniel.Raven/OneDrive - Vertex, Inc/Documents/myStuff/WM/Python Pairing Process"
+DIRECTORY = os.getcwd()
+
+SCENARIO_MAP = {
+    '1': "1 - Recon",
+    '2': "2 - Battle Lines",
+    '3': "3 - Wolves At Our Heels",
+    '4': "4 - Payload",
+    '5': "5 - Two Fronts",
+    '6': "6 - Invasion"}
+
+SCENARIO_TO_CSV_MAP = {
+    0: 1,
+    1: 7,
+    2: 13,
+    3: 19,
+    4: 25,
+    5: 31,
+    6: 37
+}
